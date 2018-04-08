3bddcb7094e297656e920cc51d8e4037b4c885f1
niragmehta <nsmehta@sfu.ca>
2018-04-08 16:24:28 -0700

    start from here

--- src/main/java/ca/coursePlanner/controller/CoursePlannerController.java
+++ src/main/java/ca/coursePlanner/controller/CoursePlannerController.java
@@ -1,20 +1,17 @@
 package ca.coursePlanner.controller;
 
 
-import ca.coursePlanner.model.DepartmentToDeleteLater;
 import ca.coursePlanner.model.Facade;
 import ca.coursePlanner.model.TxtWriter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class CoursePlannerController {
 
-    @GetMapping("/api/dump-model")
+    @GetMapping("/dump-model")
     public String summarizeData() throws FileNotFoundException {
         Facade.writeDump();
         TxtWriter txtWriterInstance = TxtWriter.getInstance();
@@ -23,23 +20,12 @@ public class CoursePlannerController {
 
     @GetMapping("/api/about")
     public Object getAboutMessage() {
-        return new Object() {
+        return new Object(){
             public String appName = "Course Planner";
             public String authorName = "Nirag and Warren";
         };
     }
 
-    @GetMapping("/api/departments")
-    public List<DepartmentToDeleteLater> getDepartments() throws FileNotFoundException {
-//        Facade.populateDepartmentList();
-//        List<DepartmentToDeleteLater> departmentToDeleteLaters = DepartmentCollection.getInstance().getDepartmentToDeleteLaterList();
 
-        List<DepartmentToDeleteLater> departmentToDeleteLaters = new ArrayList<>();
-        DepartmentToDeleteLater dept1 = new DepartmentToDeleteLater(1, "CMPT");
-        DepartmentToDeleteLater dept2 = new DepartmentToDeleteLater(2, "BUS");
 
-        departmentToDeleteLaters.add(dept1);
-        departmentToDeleteLaters.add(dept2);
-        return departmentToDeleteLaters;
-    }
 }
